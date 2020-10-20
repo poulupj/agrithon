635828893ac89ca6da3e69f6927d5da286935170
poulu <poulupjoseph@gmail.com>
2020-10-20 23:17:53 +0530

    Hardcoding disease details

--- lib/util/DiseaseHandler.dart
+++ lib/util/DiseaseHandler.dart
@@ -1,19 +1,75 @@
 import 'package:agrithon/model/Disease.dart';
 
-class DiseaseHandler{
-  List<Disease> disease=List<Disease>();
-  void initDisease(){
-    disease.add(Disease(name:"aa",path:"AS1.jpeg",desc:"aa",rem:"aa"));
-    disease.add(Disease(name:"aa",path:"AS2.jpeg",desc:"aa",rem:"aa"));
-    disease.add(Disease(name:"aa",path:"AS4.jpeg",desc:"aa",rem:"aa"));
-    disease.add(Disease(name:"aa",path:"BM.jpeg",desc:"aa",rem:"aa"));
-    disease.add(Disease(name:"aa",path:"BM3.jpeg",desc:"aa",rem:"aa"));
-    disease.add(Disease(name:"aa",path:"BM4.jpeg",desc:"aa",rem:"aa"));
-    disease.add(Disease(name:"aa",path:"BS1.jpeg",desc:"aa",rem:"aa"));
-    disease.add(Disease(name:"aa",path:"BS2.jpeg",desc:"aa",rem:"aa"));
-    disease.add(Disease(name:"aa",path:"BS2.jpeg",desc:"aa",rem:"aa"));
+class DiseaseHandler {
+  List<Disease> disease = List<Disease>();
+
+  void initDisease() {
+    disease.add(Disease(
+        name: "Bacterial spot",
+        path: "AS1.jpeg",
+        desc:
+            "Bacterial spot is caused by four species of Xanthomonas and occurs worldwide wherever tomatoes are grown. Bacterial spot causes leaf and fruit spots, which leads to defoliation, sun-scalded fruit, and yield loss.",
+        rem:
+            "Transplant treatment with streptomycin, Copper sprays and other topical treatments, Plant activator sprays"));
+    disease.add(Disease(
+        name: "Black Measles",
+        path: "AS2.jpeg",
+        desc:
+            "Grapevine measles, also called esca, black measles or Spanish measles, has long plagued grape growers with its cryptic expression of symptoms and, for a long time, a lack of identifiable causal organism(s). The name 'measles' refers to the superficial spots found on the fruit ",
+        rem:
+            "Presently, there are no effective management strategies for measles. Wine grape growers with small vineyards will often have field crews remove infected fruit prior to harvest. Raisins affected by measles will be discarded during harvest or at the packing house, while table grape growers will leave affected fruit on the vine. Current research is focused on protecting pruning wounds from fungal infections to minimize suspect fungi from colonizing fresh wounds."));
+    disease.add(Disease(
+        name: "Apple scab",
+        path: "AS4.jpeg",
+        desc:
+            "Apple scab is a common disease of plants in the rose family (Rosaceae) that is caused by the ascomycete fungus Venturia inaequalis.[1] While this disease affects several plant genera, including Sorbus, Cotoneaster, and Pyrus, it is most commonly associated with the infection of Malus trees, including species of flowering crabapple, as well as cultivated apple.",
+        rem:
+            "Choose resistant varieties when possible. Rake under trees and destroy infected leaves to reduce the number of fungal spores available to start the disease cycle over again next spring. Water in the evening or early morning hours (avoid overhead irrigation) to give the leaves time to dry out before infection can occur. Spread a 3- to 6-inch layer of compost under trees, keeping it away from the trunk, to cover soil and prevent splash dispersal of the fungal spores."));
+    disease.add(Disease(
+        name: "Bacterial spot",
+        path: "AS1.jpeg",
+        desc:
+            "Bacterial spot is caused by four species of Xanthomonas and occurs worldwide wherever tomatoes are grown. Bacterial spot causes leaf and fruit spots, which leads to defoliation, sun-scalded fruit, and yield loss.",
+        rem:
+            "Transplant treatment with streptomycin, Copper sprays and other topical treatments, Plant activator sprays"));
+    disease.add(Disease(
+        name: "Black Measles",
+        path: "AS2.jpeg",
+        desc:
+            "Grapevine measles, also called esca, black measles or Spanish measles, has long plagued grape growers with its cryptic expression of symptoms and, for a long time, a lack of identifiable causal organism(s). The name 'measles' refers to the superficial spots found on the fruit ",
+        rem:
+            "Presently, there are no effective management strategies for measles. Wine grape growers with small vineyards will often have field crews remove infected fruit prior to harvest. Raisins affected by measles will be discarded during harvest or at the packing house, while table grape growers will leave affected fruit on the vine. Current research is focused on protecting pruning wounds from fungal infections to minimize suspect fungi from colonizing fresh wounds."));
+    disease.add(Disease(
+        name: "Apple scab",
+        path: "AS4.jpeg",
+        desc:
+            "Apple scab is a common disease of plants in the rose family (Rosaceae) that is caused by the ascomycete fungus Venturia inaequalis.[1] While this disease affects several plant genera, including Sorbus, Cotoneaster, and Pyrus, it is most commonly associated with the infection of Malus trees, including species of flowering crabapple, as well as cultivated apple.",
+        rem:
+            "Choose resistant varieties when possible. Rake under trees and destroy infected leaves to reduce the number of fungal spores available to start the disease cycle over again next spring. Water in the evening or early morning hours (avoid overhead irrigation) to give the leaves time to dry out before infection can occur. Spread a 3- to 6-inch layer of compost under trees, keeping it away from the trunk, to cover soil and prevent splash dispersal of the fungal spores."));
+    disease.add(Disease(
+        name: "Bacterial spot",
+        path: "AS1.jpeg",
+        desc:
+            "Bacterial spot is caused by four species of Xanthomonas and occurs worldwide wherever tomatoes are grown. Bacterial spot causes leaf and fruit spots, which leads to defoliation, sun-scalded fruit, and yield loss.",
+        rem:
+            "Transplant treatment with streptomycin, Copper sprays and other topical treatments, Plant activator sprays"));
+    disease.add(Disease(
+        name: "Black Measles",
+        path: "AS2.jpeg",
+        desc:
+            "Grapevine measles, also called esca, black measles or Spanish measles, has long plagued grape growers with its cryptic expression of symptoms and, for a long time, a lack of identifiable causal organism(s). The name 'measles' refers to the superficial spots found on the fruit ",
+        rem:
+            "Presently, there are no effective management strategies for measles. Wine grape growers with small vineyards will often have field crews remove infected fruit prior to harvest. Raisins affected by measles will be discarded during harvest or at the packing house, while table grape growers will leave affected fruit on the vine. Current research is focused on protecting pruning wounds from fungal infections to minimize suspect fungi from colonizing fresh wounds."));
+    disease.add(Disease(
+        name: "Apple scab",
+        path: "AS4.jpeg",
+        desc:
+            "Apple scab is a common disease of plants in the rose family (Rosaceae) that is caused by the ascomycete fungus Venturia inaequalis.[1] While this disease affects several plant genera, including Sorbus, Cotoneaster, and Pyrus, it is most commonly associated with the infection of Malus trees, including species of flowering crabapple, as well as cultivated apple.",
+        rem:
+            "Choose resistant varieties when possible. Rake under trees and destroy infected leaves to reduce the number of fungal spores available to start the disease cycle over again next spring. Water in the evening or early morning hours (avoid overhead irrigation) to give the leaves time to dry out before infection can occur. Spread a 3- to 6-inch layer of compost under trees, keeping it away from the trunk, to cover soil and prevent splash dispersal of the fungal spores."));
   }
-  List<Disease> get getDisease{
-  return disease;
-}
+
+  List<Disease> get getDisease {
+    return disease;
+  }
 }
